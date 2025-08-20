a55a12211f0907dd60f75090dc516ea7bc965767
Carlos App <107434698+CarlosEApp@users.noreply.github.com>
2025-08-20 13:27:08 -0300

    Add files via upload

--- web/JS/mostruario.js
+++ web/JS/mostruario.js
@@ -5,48 +5,41 @@
 const btns = document.querySelectorAll('#btnIMG button');
 
 btns[0].addEventListener('click', () => {
-  divIMGs.scrollBy({ left: 200, behavior: 'smooth' });
+divIMGs.scrollBy({ left: 200, behavior: 'smooth' });
 });
 
 btns[1].addEventListener('click', () => {
-  divIMGs.scrollBy({ left: -200, behavior: 'smooth' });
+divIMGs.scrollBy({ left: -200, behavior: 'smooth' });
 });
-
-
 const divIMGs = document.getElementById('divIMGs');
 let isDown = false;
 let startX;
 let scrollLeft;
 
 divIMGs.addEventListener('mousedown', (e) => {
-  isDown = true;
-  divIMGs.classList.add('active');
-  startX = e.pageX - divIMGs.offsetLeft;
-  scrollLeft = divIMGs.scrollLeft;
+isDown = true;
+divIMGs.classList.add('active');
+startX = e.pageX - divIMGs.offsetLeft;
+scrollLeft = divIMGs.scrollLeft;
 });
-
 divIMGs.addEventListener('mouseleave', () => {
-  isDown = false;
-  divIMGs.classList.remove('active');
+isDown = false;
+divIMGs.classList.remove('active');
 });
-
 divIMGs.addEventListener('mouseup', () => {
-  isDown = false;
-  divIMGs.classList.remove('active');
+isDown = false;
+divIMGs.classList.remove('active');
 });
-
 divIMGs.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - divIMGs.offsetLeft;
-  const walk = (x - startX) * 2; // Velocidade de rolagem
-  divIMGs.scrollLeft = scrollLeft - walk;
+if (!isDown) return;
+e.preventDefault();
+const x = e.pageX - divIMGs.offsetLeft;
+const walk = (x - startX) * 2; // Velocidade de rolagem
+divIMGs.scrollLeft = scrollLeft - walk;
 });
-
 
 //codigo de entrada
 var codigo =sessionStorage.getItem('Mostruario')
-
 var firebaseConfig = {
 apiKey: "AIzaSyDZXtGGNJwRYxy8EKAj85JFGLHfLD3DMbk",
 authDomain: "rutimoveis-bc114.firebaseapp.com",
@@ -58,66 +51,58 @@ measurementId: "G-K330CH24NV"
 };
 firebase.initializeApp(firebaseConfig);
 var db=firebase.firestore()
-
 db.collection('Cadastros').doc(codigo).get().then((doc) => {
-    var data= doc.data()
-    //alert(data.IMG4)
-    document.getElementById("OBSText").innerHTML=data.End
-
-     document.getElementById('tituloH2').innerHTML=data.Titulo
-     document.getElementById('codigoH2').innerHTML=data.Código
-
-    document.getElementById('img1').src= data.IMG1;
-    if(!data.Img1T||data.Img1T==''){
-    document.getElementById('h3img1').innerHTML="#Rutimóveis"
-    }else{
-      document.getElementById('h3img1').innerHTML=data.Img1T
-    }
-
-    document.getElementById('img2').src=data.IMG2;
-     if(!data.Img2T||data.Img2T==''){
-     document.getElementById('h3img2').innerHTML="#Rutimóveis"
-    }else{
-        document.getElementById('h3img2').innerHTML=data.Img2T
-    }
-
-    document.getElementById('img3').src=data.IMG3;
-    if(!data.Img3T||data.Img3T==''){
-     document.getElementById('h3img3').innerHTML="#Rutimóveis"
-    }else{
-        document.getElementById('h3img3').innerHTML=data.Img3T
-    }
-   
-      document.getElementById('img4').src=data.IMG4;
-      if(!data.Img4T||data.Img4T==''){
-     document.getElementById('h3img4').innerHTML="#Rutimóveis"
-    }else{
-        document.getElementById('h3img4').innerHTML=data.Img4T
-    }
-        
-
-              document.getElementById('img5').src=data.IMG5;
-            if(!data.Img5T||data.Img5T==''){
-     document.getElementById('h3img5').innerHTML="#Rutimóveis"
-    }else{
-        document.getElementById('h3img5').innerHTML=data.Img5T
-    }
-                    
-                  document.getElementById('img6').src=data.IMG6;
-          if(!data.Img6T||data.Img6T==''){
-     document.getElementById('h3img6').innerHTML="#Rutimóveis"
-    }else{
-        document.getElementById('h3img6').innerHTML=data.Img6T
-    }
-
-                       document.getElementById('img7').src=data.IMG7;
-                    if(!data.Img7T||data.Img7T==''){
-     document.getElementById('h3img7').innerHTML="#Rutimóveis"
-    }else{
-        document.getElementById('h3img7').innerHTML=data.Img7T
-    }
-
-    
+var data= doc.data()
+//alert(data.IMG4)
+if(!data.End||data.End==''){
+document.getElementById("OBSText").innerHTML='Sem mais!'
+}else{
+document.getElementById("OBSText").innerHTML=data.End
+}
+document.getElementById('tituloH2').innerHTML=data.Titulo
+document.getElementById('codigoH2').innerHTML=data.Código
+document.getElementById('img1').src= data.IMG1;
+if(!data.Img1T||data.Img1T==''){
+document.getElementById('h3img1').innerHTML="#Rutimóveis"
+}else{
+document.getElementById('h3img1').innerHTML=data.Img1T
+}
+document.getElementById('img2').src=data.IMG2;
+if(!data.Img2T||data.Img2T==''){
+document.getElementById('h3img2').innerHTML="#Rutimóveis"
+}else{
+document.getElementById('h3img2').innerHTML=data.Img2T
+}
+document.getElementById('img3').src=data.IMG3;
+if(!data.Img3T||data.Img3T==''){
+document.getElementById('h3img3').innerHTML="#Rutimóveis"
+}else{
+document.getElementById('h3img3').innerHTML=data.Img3T
+}
+document.getElementById('img4').src=data.IMG4;
+if(!data.Img4T||data.Img4T==''){
+document.getElementById('h3img4').innerHTML="#Rutimóveis"
+}else{
+document.getElementById('h3img4').innerHTML=data.Img4T
+}
+document.getElementById('img5').src=data.IMG5;
+if(!data.Img5T||data.Img5T==''){
+document.getElementById('h3img5').innerHTML="#Rutimóveis"
+}else{
+document.getElementById('h3img5').innerHTML=data.Img5T
+}
+document.getElementById('img6').src=data.IMG6;
+if(!data.Img6T||data.Img6T==''){
+document.getElementById('h3img6').innerHTML="#Rutimóveis"
+}else{
+document.getElementById('h3img6').innerHTML=data.Img6T
+}
+document.getElementById('img7').src=data.IMG7;
+if(!data.Img7T||data.Img7T==''){
+document.getElementById('h3img7').innerHTML="#Rutimóveis"
+}else{
+document.getElementById('h3img7').innerHTML=data.Img7T
+}
 
 document.getElementById('rua').innerHTML=`Rua: ${data.Rua}`
 document.getElementById('numero').innerHTML=`Nº: ${data.Numero}`
@@ -125,112 +110,86 @@ document.getElementById('bairro').innerHTML=`Bairro: ${data.Bairro}`
 document.getElementById('cidade').innerHTML=`Cidade: ${data.Cidade}`
 document.getElementById('uf').innerHTML=`UF:(${data.UF})`
 if(!data.ValorC||data.ValorC==''){
-
 } else{
-  document.getElementById('venda').innerHTML=`Compra: ${data.ValorC} R$`
+document.getElementById('venda').innerHTML=`Compra: ${data.ValorC} R$`
 }
 if(!data.ValorA||data.ValorA==''){
-
 }else{
 document.getElementById('locaçao').innerHTML=`Locação: ${data.ValorA} R$`
 }
 if(!data.VCond||data.VCond==''){
-
 }else{
 document.getElementById('condominio').innerHTML=`Condomínio: ${data.VCond} R$`
 }
 if(!data.Viptu||data.Viptu==''){
-
 }else{
 document.getElementById('iptu').innerHTML=`IPTU: ${data.Viptu} R$`
 }
 if(!data.AreaM||data.AreaM==''){
-
 }else{
 document.getElementById('areaConst').innerHTML=`Area construida: ${data.AreaM} m²`
 }
 if(!data.Suite||data.Suite==''){
-
 }else{
 document.getElementById('suite').innerHTML=`Suíte: 🛏️(${data.Suite}) `
 }
 if(!data.Quartos||data.Quartos==''){
-
 }else{
 document.getElementById('quartos').innerHTML=`Quartos: 🛏️(${data.Quartos}) `
 }
 if(!data.Banheiro||data.Banheiro==''){
-
 }else{
 document.getElementById('banheiro').innerHTML=`Banheiros: 🚿(${data.Banheiro})`
 }
 if(!data.Quintal||data.Quintal==''){
-
 }else{
-  if(!data.QuintalM||data.QuintalM==''){
+if(!data.QuintalM||data.QuintalM==''){
 document.getElementById('quintal').innerHTML=`Quintal: (${data.Quintal}) `
 }else{
-
 document.getElementById('quintal').innerHTML=`Quintal: (${data.Quintal}) ${data.QuintalM}m² `
 }
 }
 if(!data.Garagem||data.Garagem==''){
-
 }else{
 document.getElementById('garagem').innerHTML=`Garagem: 🚗(${data.Garagem}) `
 }
 if(!data.Piscina||data.Piscina==''){
-
 }else{
-  if(!data.PiscinaM||data.PiscinaM==''){
-
+if(!data.PiscinaM||data.PiscinaM==''){
 document.getElementById('piscina').innerHTML=`Piscina: (${data.Piscina}) `
 }else{
-
 document.getElementById('piscina').innerHTML=`Piscina: (${data.Piscina}) ${data.PiscinaM}m² `
 }
 }
-  if(data.Duplex=='sim'){
+if(data.Duplex=='sim'){
 document.getElementById('duplex').innerHTML=`[Duplex] `
 }else{
 }
 if(data.Triplex=='sim'){
- 
 document.getElementById('triplex').innerHTML=`[Triplex] `
 }else{
 }
 if(!data.Desconto||data.Desconto==''){
- 
-
 }else{
-
-  document.getElementById('desconto').innerHTML=`Avista desconto de (${data.Desconto})% `
+document.getElementById('desconto').innerHTML=`Avista desconto de (${data.Desconto})% `
 }
-
-
-
-
 }).catch((err) => {
-    
+
 });
 
 //Botão WhatsApp
-
 function corretora(){
-  var telefone=`11948865333`
-   var Titulo_=document.getElementById('tituloH2').innerHTML;
-    var codigo_=document.getElementById('codigoH2').innerHTML;
-     var IMG_=document.getElementById('img1').src;
-     var pag="https://rutimoveis.netlify.app/"
-     var result= `Pagina Web: ${pag}\n\n${Titulo_} -Imagem:  ${IMG_} `
+var telefone=`11948865333`
+var Titulo_=document.getElementById('tituloH2').innerHTML;
+var codigo_=document.getElementById('codigoH2').innerHTML;
+var IMG_=document.getElementById('img1').src;
+var pag="https://rutimoveis.netlify.app/"
+var result= `Pagina Web: ${pag}\n\n${Titulo_} -Imagem:  ${IMG_} `
 var numero = `+55${telefone}`; // Substitua pelo número de destino, incluindo o código do país
 var url = "https://wa.me/"+`${numero}?text= Imagem  ${encodeURIComponent(result)} - Rute corretora (cod: ${codigo_})  `;
 window.open(url, "_blank");
 Swal.fire(`WhatsApp`,``,'success') 
 }
-
-
-
 
 //Alerta iniciar
 function AlertaInicial(){
@@ -253,7 +212,6 @@ document.body.style.paddingRight = '0px';
 });
 setTimeout(function(){
 Swal.close()
-
 },7000)
 }
 AlertaInicial()
